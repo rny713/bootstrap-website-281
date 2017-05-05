0e84a9a0464da406c9d1c9f49076be9776cd6bcf
rny713 <tech20171@gmail.com>
2017-05-05 22:09:44 +0800

    start bootstrap

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function () {
+    $('nav a').bind('click', function (event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+
+});
